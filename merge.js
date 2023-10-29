ff4b1076ae5fe7258861682bdbfaf1bd46517f2d
Akhilesh Soni <akhileshsoni098@gmail.com>
2023-10-29 15:38:27 +0530

    newCommit

--- merge.js
+++ merge.js
@@ -1,4 +1,4 @@
-const PDFMerger = require('pdf-merger-js');
+/* const PDFMerger = require('pdf-merger-js');
 
 var merger = new PDFMerger();
 
@@ -13,4 +13,34 @@ console.log(d)
  
 }
    
-module.exports = { mergePdfs }
+module.exports = { mergePdfs } */
+
+
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require('fs');
+const PDFMerger = require('pdf-merger-js');
+
+var merger = new PDFMerger();
+
+// Create the 'public' directory if it doesn't exist
+const publicDirectory = path.join(__dirname, 'public');
+if (!fs.existsSync(publicDirectory)) {
+  fs.mkdirSync(publicDirectory);
+}
+
+const mergePdfs = async (p1, p2) => {
+  await merger.add(p1);
+  await merger.add(p2);
+  let d = new Date().getTime();
+  console.log(d);
+  await merger.save(`public/${d}.pdf`);
+  return d;
+}
+ 
+module.exports = { mergePdfs };
+
+
+
+
